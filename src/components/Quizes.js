1ca75663119d5eeadb5d56f026a57d1a5d1381db
saifsammy <saifsammy653@gmail.com>
2022-10-15 13:48:16 +0600

    button added

--- src/components/Quizes.js
+++ src/components/Quizes.js
@@ -1,21 +1,26 @@
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-
 
 const Quizes = ({ data, index }) => {
     const { options, question, correctAnswer } = data
 
-    // const [correctAnswerCount, setCorrectAnswerCount] = useState(correctAnswer)
+    // console.log(data);
+    // data.options.map(option=>console.log(option))
 
-    const handleAddToCart = (e) => {
-        const result = e.target.value;
+
+    // question.options.map(option => { })
+    // onClick = {() => handle()}
+
+    const handleAddToCart = (index) => {
+        const result = index;
+        console.log(result)
         if (result === correctAnswer) {
-            console.log('correct')
+            // console.log(result,index)
+            // alert('correct')
         } else {
-            console.log('wrong')
+            // console.log(result,index)
+            // alert('wrong')
         }
-        console.log(e)
         // toast.info('Info: Product Added!', { autoClose: 500 })
     }
 
@@ -30,15 +35,14 @@ const Quizes = ({ data, index }) => {
             <section className="py-6 dark:bg-gray-800 text-white">
                 <div className="container flex flex-col justify-center p-4 mx-auto">
                     <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
-                        <button onClick={e => handleAddToCart(e)}
-                            className='bg-indigo-500 rounded-lg hover:bg-amber-500 p-5'>{options[0]}</button>
-                        <button onClick={e => handleAddToCart(e)}
-                            className='bg-indigo-500 rounded-lg hover:bg-amber-500 p-5'>{options[1]}</button>
-                        <button onClick={onchange = e => handleAddToCart(e)}
-                            className='bg-indigo-500 rounded-lg hover:bg-amber-500 p-5'>{options[2]}</button>
-                        <button onClick={e => handleAddToCart(e)}
-                            className='bg-indigo-500 rounded-lg hover:bg-amber-500 p-5'>{options[3]}</button>
-
+                        
+                        {
+                            data.options.map(option =>  <button
+                                onClick={()=>handleAddToCart(index,{correctAnswer})}
+                                className='bg-indigo-500 rounded-lg hover:bg-amber-500 p-5'
+                            >{option}</button>)
+                        }
+                        
                     </div>
                 </div>
             </section>
